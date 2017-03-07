92bf74df1e620e0047433ffda32dba3e204a526e
Marcos Freire <marcosdefontes@gmail.com>
2017-03-07 17:17:11 -0300

    Estilos relacionamentos

--- resources/views/membro/edit-relacionamentos.blade.php
+++ resources/views/membro/edit-relacionamentos.blade.php
@@ -1,12 +1,10 @@
-<div class="row">
-    <div class="col-md-12">
-        <hr class="divider">
-    </div>
+    <hr class="divider">
+
     <div class="relacionamentos">
 
         <h3>Relacionamentos</h3>
 
-        <h4>Família</h4>
+        <h4><i class="fa fa-home" aria-hidden="true"></i> Família</h4>
         <p ng-show="relacionamentosFamilia.length == 0 ">
             Nenhum relacionamento familiar cadastrado.
         </p>
@@ -14,6 +12,7 @@
             <li ng-repeat="rel in relacionamentosFamilia track by rel.id"
             class="relacionamento"
             ng-class="rel.relacionamento.desc_geral">
+                <i class="fa fa-link" aria-hidden="true"></i>
                 <span class="membro-de"><%membro.nome%></span> é
                 <span class="relacionamento-desc" ng-class="rel.relacionamento.desc_geral"
                 ng-bind="membro.sexo == 'M' ? rel.relacionamento.desc_masc : rel.relacionamento.desc_fem"></span>
@@ -22,7 +21,7 @@
                     <a href="<%userShowLink(rel.membro_para.id)%>">
                         <%rel.membro_para.nome%></a>.
                 </span>
-                <a href="#" ng-click="actRemoveRelacionamento(membro.id,rel.id)">
+                <a href="#" title="Remover relacionamento" class="remover-relacionamento" ng-click="actRemoveRelacionamento(membro.id,rel.id)">
                     <i class="fa fa-times" aria-hidden="true" ></i>
                 </a>
             </li>
@@ -46,9 +45,6 @@
             	typeahead-no-results="semResultadosMembrosRelFamilia"
             	class="">
             <i ng-show="carregandoMembrosRelFamilia" class="fa fa-circle-o-notch fa-spin fa-fw"></i>
-            <div ng-show="semResultadosMembrosRelFamilia">
-              <i class="fa fa-times" aria-hidden="true"></i> Nenhum membro encontrado
-            </div>
             <button
                 class="btn btn-info"
                 ng-disabled="id_rel_familia_selected == null"
@@ -59,6 +55,7 @@
                     ;id_rel_familia_selected=null;add_tipo_relacionamento=null">
                 <i class="fa fa-plus-square" aria-hidden="true"></i>
             </button>
+            <span class="no-results" ng-show="semResultadosMembrosRelFamilia">Nenhum membro encontrado</span>
 
         </div>
 
@@ -71,7 +68,7 @@
 
 
 {{-- +++++++++++++++++++ Igreja +++++++++++++++++++ --}}
-<h4>Igreja</h4>
+<h4><i class="fa fa-users" aria-hidden="true"></i> Igreja</h4>
 <p ng-show="relacionamentosIgreja.length == 0 ">
     Nenhum relacionamento na igreja cadastrado.
 </p>
@@ -79,6 +76,7 @@
     <li ng-repeat="relac in relacionamentosIgreja track by relac.id"
     class="relacionamento"
     ng-class="relac.desc_geral">
+        <i class="fa fa-link" aria-hidden="true"></i>
         <span class="membro-de"><%membro.nome%></span> é
         <span class="relacionamento-desc" ng-class="relac.relacionamento.desc_geral"
         ng-bind="membro.sexo == 'M' ? relac.relacionamento.desc_masc : relac.relacionamento.desc_fem"></span>
@@ -87,7 +85,7 @@
             <a href="<%userShowLink(relac.membro_para.id)%>">
                 <%relac.membro_para.nome%></a>.
         </span>
-        <a href="#" ng-click="actRemoveRelacionamento(membro.id,relac.id)">
+        <a href="#" title="Remover relacionamento" class="remover-relacionamento" ng-click="actRemoveRelacionamento(membro.id,relac.id)">
             <i class="fa fa-times" aria-hidden="true" ></i></a>
 
     </li>
@@ -111,9 +109,6 @@
         typeahead-no-results="semResultadosMembrosRelIgreja"
         class="">
     <i ng-show="carregandoMembrosRelIgreja" class="fa fa-circle-o-notch fa-spin fa-fw"></i>
-    <div ng-show="semResultadosMembrosRelIgreja">
-      <i class="fa fa-times" aria-hidden="true"></i> Nenhum membro encontrado
-    </div>
     <button
         class="btn btn-info"
         ng-disabled="id_rel_igreja_selected == null"
@@ -122,8 +117,7 @@
         ;id_rel_igreja_selected=null;add_tipo_relacionamento_igreja=null">
         <i class="fa fa-plus-square" aria-hidden="true"></i>
     </button>
-
-</div>
+    <span class="no-results" ng-show="semResultadosMembrosRelIgreja">Nenhum membro encontrado</span>
 
 </div>
 
