19358b50c15c55f5993cc4f4676544e19cbda4ea
Marcos Freire <marcosdefontes@gmail.com>
2017-03-13 13:45:34 -0300

    Inclusão de rotas

--- app/Http/Routes/RoutesMembros.php
+++ app/Http/Routes/RoutesMembros.php
@@ -3,16 +3,18 @@
 
 Route::group(['middleware' => 'web'], function () {
 
-    //Route::resource('membro/grupo-caseiro','membro\GrupoCaseiroController');
-    //
+    // Grupo caseiro
     Route::get('membro/grupo-caseiro','membro\GrupoCaseiroController@index')
         ->name('membros.grupo-caseiro');
     Route::get('membro/grupo-caseiro/lista','membro\GrupoCaseiroController@lista')
         ->name('membros.grupo-caseiro.lista');
     Route::post('membro/grupo-caseiro','membro\GrupoCaseiroController@salvar');
 
+    // Relacionamentos
     Route::post('membro/remover-relacionamento','membro\RelacionamentoController@removeRelacionamento');
     Route::post('membro/{membro}/relacionamento/add','membro\RelacionamentoController@addRelacionamento');
+
+    // Membros
     Route::resource('membro','membro\MembroController');
     Route::get('membro/{membro}/relacionamentos/{categoria?}', 'membro\RelacionamentoController@relacionamentosMembro');
     Route::get('membros', 'membro\MembroController@lista')
