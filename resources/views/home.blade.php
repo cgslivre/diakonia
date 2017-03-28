70a1484ba21e9ac581f84c9f53ec7cfeff937e60
Marcos Freire <marcosdefontes@gmail.com>
2017-03-28 16:18:02 -0300

    Página Dashboard (esqueleto)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,6 @@
-@extends('layouts.app')
+@extends( 'master')
+@section('nivel1', '<li><a href="/membros">Início</a></li>')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
-
-                    <div class="panel-body">
-                        You are logged in!
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+Dashboard
 @endsection
