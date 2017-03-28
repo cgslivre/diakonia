3340aa2bf3edb6ed7cf9e29eb5ae6a6ce2505aeb
Marcos Freire <marcosdefontes@gmail.com>
2017-03-28 17:33:03 -0300

    Modelo consulta_membros

---
+++ database/migrations/2017_03_28_171327_tabela-consultas-membros.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TabelaConsultasMembros extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consulta_membros', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('slug')->unique();
+            $table->string('titulo');
+            $table->smallInteger('idade_minima')->nullable();
+            $table->smallInteger('idade_maxima')->nullable();
+            $table->char('tem_discipulos',1)->nullable();
+            $table->char('tem_discipulador',1)->nullable();
+            $table->char('sexo',1)->nullable();
+            $table->string('regioes')->nullable();
+            $table->string('grupos')->nullable();
+
+
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->foreign('created_by')
+                ->references('id')
+                ->on('users');
+
+            $table->integer('modified_by')->unsigned()->nullable();
+            $table->foreign('modified_by')
+                ->references('id')
+                ->on('users');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('consulta_membros');
+    }
+}
