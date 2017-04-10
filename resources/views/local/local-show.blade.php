1c5762c34eac3c7c1eb3d23359d4c7e8624d9cd9
Marcos Freire <marcosdefontes@gmail.com>
2017-04-10 14:35:14 -0300

    CRUD Locais

--- resources/views/local/local-show.blade.php
+++ resources/views/local/local-show.blade.php
@@ -16,8 +16,6 @@
     marginheight="0" marginwidth="0"
     src="https://maps.google.com/maps?q={{$local->localizacaoJson->latitude}},{{$local->localizacaoJson->longitude}}&hl=pt;z=14&amp;output=embed">
 </iframe><br />
-<small>
-    <a href="https://maps.google.com/maps?q={{$local->localizacaoJson->latitude}},{{$local->localizacaoJson->longitude}}&hl=es;z=14&amp;output=embed"
-        style="color:#0000FF;text-align:left" target="_blank">Ver mapa maior</a>
-</small>
+<a href="https://maps.google.com/maps?q={{$local->localizacaoJson->latitude}},{{$local->localizacaoJson->longitude}}&hl=es;z=14&amp;output=embed"
+    target="_blank">Ver mapa maior</a>
 @endsection
