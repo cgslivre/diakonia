354e9d1da0e0f69d545e09ba831041c4c77f4e20
Marcos Freire <marcosdefontes@gmail.com>
2017-04-12 15:43:56 -0300

    Visualização Evento

--- resources/views/evento/evento-show.blade.php
+++ resources/views/evento/evento-show.blade.php
@@ -1,19 +1,59 @@
+{{ Date::setLocale('pt_BR') }}
 @extends( 'evento.template-evento')
 
-@section('nivel2')<li class="active">{{$evento->titulo}}</li>@stop
+@section('nivel2')<li class="active">{{$evento->titulo}} -
+    {{Date::parse($evento->data_hora_inicio)->format('j \d\e F \d\e Y')}}</li>@stop
 
 
 @section('content')
-<h3>
-    {{$evento->titulo}}
-</h3>
+<div class="show-evento">
+    <div class="titulo">
+        <h3>
+            {{$evento->titulo}}
+        </h3>
+    </div>
 
-{{--
-<iframe width="90%" height="400px" frameborder="0" scrolling="no"
-    marginheight="0" marginwidth="0"
-    src="https://maps.google.com/maps?q={{$local->localizacaoJson->latitude}},{{$local->localizacaoJson->longitude}}&hl=pt;z=14&amp;output=embed">
-</iframe><br />
-<a href="https://maps.google.com/maps?q={{$local->localizacaoJson->latitude}},{{$local->localizacaoJson->longitude}}&hl=es;z=14&amp;output=embed"
-    target="_blank">Ver mapa maior</a>
-    --}}
+    <p class="card">
+        <span class="datas">
+            Data/Hora Início <span>{{Date::parse($evento->data_hora_inicio)
+                ->format('l, j \d\e F \d\e Y, \à\s G:i')}}</span></span>
+        <span class="datas">
+            Data/Hora Fim <span>{{Date::parse($evento->data_hora_fim)
+                ->format('l, j \d\e F \d\e Y, \à\s G:i')}}</span>
+        </span>
+    </p>
+
+    <p class="card duracao">
+        <span class="duracao">
+            Duração <span>{{$evento->data_hora_inicio->diffForHumans(
+                $evento->data_hora_fim,true)}}
+            </span></span>
+        <?php
+            $today = new \Carbon\Carbon();
+         ?>
+        @if( $evento->data_hora_inicio > $today)
+        <span class="comecaem">
+            Começa em <span>{{$evento->data_hora_inicio->diffForHumans(null,true)}}</span>
+        </span>
+        @endif
+
+    </p>
+
+    <p>
+        <small>Criado em:
+            <strong>
+                {{Date::parse($evento->created_at)->format('j/n/Y G:i')}}
+            </strong>, por
+            <strong>{{$evento->createdBy->name}}</strong>.
+        </small>
+    </p>
+    <p>
+        <small>Última alteração:
+            <strong>
+                {{Date::parse($evento->updated_at)->format('j/n/Y G:i')}}
+            </strong>, por
+            <strong>{{$evento->updatedBy->name}}</strong>.
+        </small>
+    </p>
+</div>
 @endsection
