764d5637231d79a8acbb3a897cf457bacf13b019
Marcos Freire <marcosdefontes@gmail.com>
2017-04-20 16:18:43 -0300

    Estilo Email

--- resources/views/emails/novo-usuario.blade.php
+++ resources/views/emails/novo-usuario.blade.php
@@ -3,14 +3,6 @@
 
 The body of your message.
 
-@component('mail::panel')
-Painel
-@endcomponent
 
-@component('mail::button', ['url' => ''])
-Button Text
-@endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
 @endcomponent
