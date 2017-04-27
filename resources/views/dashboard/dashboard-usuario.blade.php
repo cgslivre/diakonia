c2f1bb15e8b905019b2c517874208d857420c920
Marcos Freire <marcosdefontes@gmail.com>
2017-04-27 18:46:23 -0300

    Dashboards usuários

--- resources/views/dashboard/dashboard-usuario.blade.php
+++ resources/views/dashboard/dashboard-usuario.blade.php
@@ -5,10 +5,10 @@
             aqui</a>
     </div>
 @endif
+@if(isset($data["usuario.usuarios-sem-perfil"]))
 <div class="panel panel-default panel-dashboard">
   <div class="panel-heading">Usuários</div>
   <div class="panel-body">
-      @if(isset($data["usuario.usuarios-sem-perfil"]))
           <h4>Usuários sem perfis definidos</h4>
           <p>Os usuário abaixo ainda não possuem nenhum perfil definido, clique nos nomes para
           definir um perfil.</p>
@@ -16,10 +16,10 @@
               @forelse ($data["usuario.usuarios-sem-perfil"] as $usuario)
                   <li><a href="{{ URL::route('usuario.permissoes.edit', $usuario->id) }}">{{$usuario->name}} - {{$usuario->email}}</a></li>
               @empty
-                  <li>Todos usuário com pelo menos um perfil definido!</li>
+                  <li>Todos usuários com pelo menos um perfil definido!</li>
               @endforelse
           </ul>
 
-      @endif
   </div>
 </div>
+@endif
