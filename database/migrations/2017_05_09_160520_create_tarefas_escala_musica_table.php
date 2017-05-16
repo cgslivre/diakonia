07ee7b7dfdf14fcfa295b2c56cda4f0a9d41564e
Marcos Freire <marcosdefontes@gmail.com>
2017-05-16 13:58:41 -0300

    Ajuste de gitignore

--- database/migrations/2017_05_09_160520_create_tarefas_escala_musica_table.php
+++ database/migrations/2017_05_09_160520_create_tarefas_escala_musica_table.php
@@ -16,8 +16,6 @@ class CreateTarefasEscalaMusicaTable extends Migration
         Schema::create('tarefas_escala_musica', function (Blueprint $table) {
             $table->increments('id');
             $table->timestamps();
-            $table->string('token',60);
-            $table->index('token');
 
             $table->integer('escala_id')->unsigned()->index();
             $table->foreign('escala_id')
