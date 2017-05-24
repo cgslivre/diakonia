ed249b66d58b7ba128ead62e7b49c3a15d8f79b9
Marcos Freire <marcosdefontes@gmail.com>
2017-05-24 11:38:58 -0300

    Tema modal de ajuda da escala

--- resources/views/musica/escala/modal-ajuda.blade.php
+++ resources/views/musica/escala/modal-ajuda.blade.php
@@ -4,9 +4,22 @@
             <div class="modal-header modal-header-info">
                 <h5 class="modal-title">Ajuda</h4>
             </div>
-            <div class="modal-body">
-                Confirma o impedimento para participar
-                da escala no dia <span id="c_imp_dt_evento"></span>?
+            <div class="modal-body ajuda">
+                <h4>Status da Escala:</h4>
+                <div class="escala-info">
+                    <div class="status-escala escala-criada">
+                        <i class="fa fa-exclamation-triangle" aria-hidden="true"
+                            title="Escala não publicada"></i>
+                    </div>
+                    Escala salva, porém <strong>não</strong> publicada;
+                </div>
+                <div class="escala-info">
+                    <div class="status-escala escala-criada">
+                        <i class="fa fa-exclamation-triangle" aria-hidden="true"
+                            title="Escala não publicada"></i>
+                    </div>
+                    Escala salva, porém <strong>não</strong> publicada;
+                </div>
             </div>
 
         </div>
