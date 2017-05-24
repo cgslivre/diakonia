37150e281ab44e3f0d3cc23e483f1db0cb284ca9
Marcos Freire <marcosdefontes@gmail.com>
2017-05-24 11:58:06 -0300

    Modal de ajuda para escala

--- resources/views/musica/escala/modal-ajuda.blade.php
+++ resources/views/musica/escala/modal-ajuda.blade.php
@@ -7,20 +7,104 @@
             <div class="modal-body ajuda">
                 <h4>Status da Escala:</h4>
                 <div class="escala-info">
-                    <div class="status-escala escala-criada">
-                        <i class="fa fa-exclamation-triangle" aria-hidden="true"
-                            title="Escala não publicada"></i>
+                    <div class="status-escala sem-escala">
+                        <i class="fa fa-exclamation-circle" aria-hidden="true"
+                            title="Sem Escala"></i>
                     </div>
-                    Escala salva, porém <strong>não</strong> publicada;
+                    Evento <strong>sem</strong> escala.
                 </div>
                 <div class="escala-info">
                     <div class="status-escala escala-criada">
                         <i class="fa fa-exclamation-triangle" aria-hidden="true"
                             title="Escala não publicada"></i>
                     </div>
-                    Escala salva, porém <strong>não</strong> publicada;
+                    Escala salva, porém <strong>não</strong> publicada.
                 </div>
-            </div>
+                <div class="escala-info">
+                    <div class="status-escala escala-publicada">
+                        <i class="fa fa-check-circle" aria-hidden="true"
+                            title="Escala publicada"></i>
+                    </div>
+                    Escala salva e <strong>publicada</strong>.
+                </div>
+
+                <h4>Participação na escala:</h4>
+                <div class="escala-info">
+                    <div class="escalado">
+                        <i class="fa fa-hand-peace-o" aria-hidden="true"
+                            title="Você está escalado"></i>
+                    </div>
+                    Você está escalado para este evento
+                </div>
+                <div class="escala-info">
+                    <div class="lider">
+                        <i class="fa fa-star" aria-hidden="true"
+                            title="Você é o líder da escala"></i>
+                    </div>
+                    Você é o <strong>líder</strong> da escala.
+                </div>
+                <div class="escala-info">
+                    <div class="impedimento">
+                        <i class="fa fa-hand-paper-o" aria-hidden="true"
+                            title="Nem todos colaboradores podem particiar desta escala"></i>
+                    </div>
+                    Nem todos colaboradores podem particiar desta escala.
+                </div>
+                <div class="escala-info">
+                    <div class="usuario-impedido">
+                        <i class="fa fa-thumbs-down" aria-hidden="true"
+                            title="Você declarou-se impedido de participar desta escala"></i>
+                    </div>
+                    Você declarou-se impedido de participar desta escala
+                </div>
+
+                <h4>Ações na escala:</h4>
+                <div class="escala-info">
+                    <div class="btn-acao">
+                        <i class="fa fa-cog"></i>
+                    </div>
+                    Botão para exibir ações disponíveis para a escala
+                </div>
+
+                <div class="escala-info">
+                    <div class="toolbar-btn-ajuda">
+                        <i class="fa fa-calendar-o"></i>
+                    </div>
+                    Ver detalhes do evento
+                </div>
+                <div class="escala-info">
+                    <div class="toolbar-btn-ajuda">
+                        <i class="fa fa-plus"></i>
+                    </div>
+                    Criar escala
+                </div>
+                <div class="escala-info">
+                    <div class="toolbar-btn-ajuda">
+                        <i class="fa fa-feed"></i>
+                    </div>
+                    Publicar escala
+                </div>
+                <div class="escala-info">
+                    <div class="toolbar-btn-ajuda">
+                        <i class="fa fa-eye"></i>
+                    </div>
+                    Ver escala
+                </div>
+                <div class="escala-info">
+                    <div class="toolbar-btn-ajuda">
+                        <i class="fa fa-thumbs-down"></i>
+                    </div>
+                    Clique quando não puder participar dessa escala
+                </div>
+                <div class="escala-info">
+                    <div class="toolbar-btn-ajuda">
+                        <i class="fa fa-thumbs-up"></i>
+                    </div>
+                    Clique quando você estiver disponível para a escala (uma vez que declarou-se
+                    impedido anteriormente)
+                </div>
+
+            </div>  {{-- Fim body modal --}}
 
         </div>
     </div>
