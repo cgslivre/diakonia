b7a3f3f6bee730315f11a05f5745c042c8fee9e7
Marcos Freire <marcosdefontes@gmail.com>
2017-05-26 16:50:24 -0300

    Raw select

--- app/Http/Controllers/musica/ColaboradorMusicaController.php
+++ app/Http/Controllers/musica/ColaboradorMusicaController.php
@@ -152,8 +152,57 @@ class ColaboradorMusicaController extends Controller
     }
 
     protected function historicoReferencia($colaborador_id, $escala_id){
-        $result = DB::select( DB::raw("SELECT * FROM escalas_musica WHERE id = :escala_id"), [
-            'escala_id' => $escala_id
+        $result = DB::select(
+    DB::raw("SELECT * FROM (
+            (SELECT
+              DATE_FORMAT(e.data_hora_inicio,'%d/%m/%Y') AS data
+              , e.data_hora_inicio
+              , e.escala_musica_id
+              , CASE WHEN (SELECT COUNT(1)
+                  FROM tarefas_escala_musica t
+            	  WHERE t.escala_id = e.escala_musica_id
+                  AND t.colaborador_id = $colaborador_id) > 0 THEN 'E' ELSE 'NE' END AS escalado
+              , 'antes' AS referencia
+              FROM eventos e
+              WHERE e.escala_musica_id IS NOT NULL
+                AND e.data_hora_inicio < (SELECT data_hora_inicio FROM eventos WHERE escala_musica_id = 5)
+              ORDER BY e.data_hora_inicio DESC
+              LIMIT 5
+              )
+            UNION
+            (
+            SELECT
+            	DATE_FORMAT(e.data_hora_inicio,'%d/%m/%Y') AS data
+                , e.data_hora_inicio
+                , e.escala_musica_id
+                , CASE WHEN (SELECT COUNT(1)
+                  FROM tarefas_escala_musica t
+            	  WHERE t.escala_id = 1
+                  AND t.colaborador_id = $colaborador_id) > 0 THEN 'E' ELSE 'NE' END AS escalado
+            	, 'atual' AS referencia
+            FROM eventos e
+            WHERE e.escala_musica_id = 5
+            )
+            UNION
+            (
+            SELECT
+              DATE_FORMAT(e.data_hora_inicio,'%d/%m/%Y') AS data
+              , e.data_hora_inicio
+              , e.escala_musica_id
+              , CASE WHEN (SELECT COUNT(1)
+                  FROM tarefas_escala_musica t
+            	  WHERE t.escala_id = e.escala_musica_id
+                  AND t.colaborador_id = $colaborador_id) > 0 THEN 'E' ELSE 'NE' END AS escalado
+              , 'depois' AS referencia
+              FROM eventos e
+              WHERE e.escala_musica_id IS NOT NULL
+                AND e.data_hora_inicio > (SELECT data_hora_inicio FROM eventos WHERE escala_musica_id = 5)
+              ORDER BY e.data_hora_inicio
+              LIMIT 5
+              )
+            ) AS C ORDER BY data_hora_inicio"), [
+            'escala_id' => $escala_id,
+            'colaborador_id' => $colaborador_id
         ]);
         return $result;
     }
