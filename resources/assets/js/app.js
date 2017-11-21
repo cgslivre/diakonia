88e24a25a248da2ea4662df872d325f0a6be5477
Marcos Freire <marcosdefontes@gmail.com>
2017-11-21 17:59:03 -0200

    Atualização bower

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,36 +1,35 @@
-
 $('.alert, hr.mensagem').not('.alert-important').delay(3000).slideUp(300);
 $(".telefone-mask").text(function(i, text) {
-        text = text.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
-        return text;
-    });
+    text = text.replace(/(\d{2})(\d{5})(\d{4})/, "($1) $2-$3");
+    return text;
+});
 
-function formatPhone( input ){
-    if( input == null || input.length == 0 ){
+function formatPhone(input) {
+    if (input == null || input.length == 0) {
         return '';
-    } else{
+    } else {
         switch (input.length) {
             case 8:
-                return input.slice(0,4) + '-' + input.slice(4,8);
+                return input.slice(0, 4) + '-' + input.slice(4, 8);
             case 9:
-                return input.slice(0,5) + '-' + input.slice(5,9);
+                return input.slice(0, 5) + '-' + input.slice(5, 9);
             case 10:
-                return '(' + input.slice(0,2) + ') ' + input.slice(2,6) + '-' + input.slice(6,10);
+                return '(' + input.slice(0, 2) + ') ' + input.slice(2, 6) + '-' + input.slice(6, 10);
             case 11:
-                return '(' + input.slice(0,2) + ') ' + input.slice(2,7) + '-' + input.slice(7,11);
+                return '(' + input.slice(0, 2) + ') ' + input.slice(2, 7) + '-' + input.slice(7, 11);
             default:
                 return input;
         }
     }
 }
 
-var comum = angular.module( 'comum', [])
+var comum = angular.module('comum', [])
 comum.filter('highlight', ['$sce', function($sce) {
     return function(text, phrase) {
 
-        if( phrase ){
-            if( text ){
-                text = text.replace(new RegExp('('+phrase+')', 'gi'),
+        if (phrase) {
+            if (text) {
+                text = text.replace(new RegExp('(' + phrase + ')', 'gi'),
                     '<span class="highlighted">$1</span>');
             }
         }
@@ -39,55 +38,55 @@ comum.filter('highlight', ['$sce', function($sce) {
 }]);
 
 comum.filter('removeAcentos', function() {
-  return function(items, criterioDeBusca) {
-    var clean = function(value) {
-        return value
-            .replace(/á/g, 'a')
-            .replace(/ã/g, 'a')
-            .replace(/â/g, 'a')
-            .replace(/ç/g, 'c')
-            .replace(/é/g, 'e')
-            .replace(/ẽ/g, 'e')
-            .replace(/í/g, 'i')
-            .replace(/ó/g, 'o')
-            .replace(/õ/g, 'o')
-            .replace(/ú/g, 'u');
+    return function(items, criterioDeBusca) {
+        var clean = function(value) {
+            return value
+                .replace(/á/g, 'a')
+                .replace(/ã/g, 'a')
+                .replace(/â/g, 'a')
+                .replace(/ç/g, 'c')
+                .replace(/é/g, 'e')
+                .replace(/ẽ/g, 'e')
+                .replace(/í/g, 'i')
+                .replace(/ó/g, 'o')
+                .replace(/õ/g, 'o')
+                .replace(/ú/g, 'u');
         }
-    var filtered = [];
-    angular.forEach(items, function(el) {
-        if( !criterioDeBusca){
-            filtered.push(el);
-        } else{
-            var full = el.nome + ' ' + el.regiao + ' '
-                + el.telefones + ' ' + el.email + ' ';
-            var grupo  = el.grupo ? el.grupo.nome : '';
-            full = full + grupo;
-            full = clean(full.toLowerCase());
-            var busca = clean( criterioDeBusca.toLowerCase());
-            if( full.indexOf(busca) > -1 ){
+        var filtered = [];
+        angular.forEach(items, function(el) {
+            if (!criterioDeBusca) {
                 filtered.push(el);
+            } else {
+                var full = el.nome + ' ' + el.regiao + ' ' +
+                    el.telefones + ' ' + el.email + ' ';
+                var grupo = el.grupo ? el.grupo.nome : '';
+                full = full + grupo;
+                full = clean(full.toLowerCase());
+                var busca = clean(criterioDeBusca.toLowerCase());
+                if (full.indexOf(busca) > -1) {
+                    filtered.push(el);
+                }
             }
-        }
-    });
-    return filtered;
-  }
+        });
+        return filtered;
+    }
 });
 
 comum.filter('formatPhone', ['$sce', function($sce) {
     return function(input) {
-        var ret = input.toString();
-        if( ret == null || ret.length == 0 ){
+        if (input) var ret = input.toString();
+        if (ret == null || ret.length == 0) {
             return '';
-        } else{
+        } else {
             switch (ret.length) {
                 case 8:
-                    return ret.slice(0,4) + '-' + ret.slice(4,8);
+                    return ret.slice(0, 4) + '-' + ret.slice(4, 8);
                 case 9:
-                    return ret.slice(0,5) + '-' + ret.slice(5,9);
+                    return ret.slice(0, 5) + '-' + ret.slice(5, 9);
                 case 10:
-                    return '(' + ret.slice(0,2) + ') ' + ret.slice(2,6) + '-' + ret.slice(6,10);
+                    return '(' + ret.slice(0, 2) + ') ' + ret.slice(2, 6) + '-' + ret.slice(6, 10);
                 case 11:
-                    return '(' + ret.slice(0,2) + ') ' + ret.slice(2,7) + '-' + ret.slice(7,11);
+                    return '(' + ret.slice(0, 2) + ') ' + ret.slice(2, 7) + '-' + ret.slice(7, 11);
                 default:
                     return ret;
             }
@@ -114,7 +113,7 @@ comum.directive("compareTo", function() {
     };
 });
 
-comum.directive("dateFormat", function(){
+comum.directive("dateFormat", function() {
     return {
         // limit usage to argument only
         restrict: 'A',
@@ -124,8 +123,8 @@ comum.directive("dateFormat", function(){
             // please note you can name your function & argument anything you like
             function customValidator(ngModelValue) {
 
-                var valid = moment(ngModelValue,'D/M/YYYY H:mm', true);
-                console.log(ngModelValue + ": " +valid.isValid());
+                var valid = moment(ngModelValue, 'D/M/YYYY H:mm', true);
+                console.log(ngModelValue + ": " + valid.isValid());
                 if (valid.isValid()) {
                     ctrl.$setValidity('formato', true);
                 } else {
