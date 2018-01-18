ffd2a6018b588cf313930a7665ca691feb0fe83b
Marcos Freire <marcosdefontes@gmail.com>
2018-01-18 13:30:38 -0200

    Pequena correção

--- app/Retiro.php
+++ app/Retiro.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Retiro extends Model
 {
-    protected $dates = ['data_inicio','data_fim']
+    protected $dates = ['data_inicio','data_fim'];
 
     protected $casts = ['grupos_disponiveis' => 'array'];
 }
