74d6aa8aa273902cc0e826751ddf172f9974cf7a
Marcos Freire <marcos.freire@serpro.gov.br>
2018-03-15 17:13:38 -0300

    materialize

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 
-mix.sass('resources/assets/sass/app.scss', 'dist/')
+mix.sass('resources/assets/sass/app.scss', 'public/css')
     .js('resources/assets/js/app.js', 'public/js');
 
 // Full API
