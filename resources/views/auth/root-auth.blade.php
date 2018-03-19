c19d00d826304dce90eb73b68bf1af270e9b8fe1
Marcos Freire <marcos.freire@serpro.gov.br>
2018-03-19 11:47:19 -0300

    Webpack com JS

--- resources/views/auth/root-auth.blade.php
+++ resources/views/auth/root-auth.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.root') 
 @section('app-scope','auth') 
 @yield('content')
-<script type="text/javascript" src="{{ mix('manifest.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/manifest.js') }}"></script>
 <script type="text/javascript" src="{{ mix('js/vendor.js') }}"></script>
+<script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
 
 mix.sass('resources/assets/sass/index.scss', 'public/css/style.css')
+    .js(['resources/assets/js/materialize.js'], 'public/js/app.js')
     .extract(['materialize-css'], 'public/js/vendor.js')
