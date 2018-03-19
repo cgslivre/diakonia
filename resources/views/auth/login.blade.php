ff656edeb65b0c7e208ba2c6c737a533876b277e
Marcos Freire <marcos.freire@serpro.gov.br>
2018-03-19 16:33:20 -0300

    Página de registro

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,9 +11,11 @@
             {!! csrf_field() !!}
             <div class="row">
                 <div class="input-field col s12">
-                    <i class="fa fa-user prefix" aria-hidden="true"></i>                    
-                    <input id="email" type="email"  class="validate {{ $errors->has('email') ? 'invalid' : '' }}" 
-                    type="email" name="email" value="{{ old('email') }}" style="color:#e8e9eb">
+                    <i class="fa fa-user prefix" aria-hidden="true"></i>                   
+                    {{--  Fix: Cor do formulário auto-preenchido no Chrome persistia na cor PRETO, 
+                    precisei definir no estilo do elemento diretamente   --}}
+                    <input id="email" type="email"  class="validate {{ $errors->has('email') ? 'invalid' : '' }} text-white" 
+                    type="email" name="email" value="{{ old('email') }}" style="color:white">
                     <label for="email">Email</label>
                     <span class="helper-text" 
                         data-error="{{ $errors->has('email') ? $errors->first('email') : 'Email inválido' }}"></span>                    
@@ -22,7 +24,7 @@
             <div class="row">
                 <div class="input-field col s12">
                     <i class="fa fa-lock prefix" aria-hidden="true"></i>
-                    <input type="password" name="password" class="validate  {{ $errors->has('password') ? 'invalid' : '' }}" style="color:#e8e9eb">
+                    <input type="password" name="password" class="validate  {{ $errors->has('password') ? 'invalid' : '' }} text-white" style="color:#e8e9eb">
                     <label for="password">Senha</label>
                     <span class="helper-text" 
                         data-error="{{ $errors->has('password') ? $errors->first('password') : 'Senha inválida' }}"></span>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,65 @@
-@extends('template-guest')
+@extends('auth.root-auth')
+@section('content')
+<div class="login-box">
+    <header id="login" class="center-align">            
+        <img src="{{asset('img/logo_v2.png')}}" alt="Logo Diakonia">
+    </header>
+
+    <header class="light-blue darken-4 white-text center-align">Registrar novo usuário</header>
+
+    <div class="login-conteudo hoverable">
+
+        <div class="row">
+            <div class="input-field col s12">
+                    <i class="fa fa-user prefix" aria-hidden="true"></i> 
+                <input name="name" type="text" class="validate white-text">
+                <label for="name">Nome</label>
+                @if ($errors->has('name'))                    
+                    <span class="helper-text" data-error="{{ $errors->first('name') }}"></span>
+                @endif
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <i class="fa fa-envelope prefix" aria-hidden="true"></i> 
+                <input name="email" type="email" class="validate white-text">
+                <label for="email">Email</label>
+                @if ($errors->has('email'))                    
+                    <span class="helper-text" data-error="{{ $errors->first('email') }}"></span>
+                @endif
+            </div>
+        </div>
+        <div class="row">
+            <div class="input-field col s12">
+                <i class="fa fa-lock prefix" aria-hidden="true"></i> 
+                <input  name="password" type="password" class="validate white-text">
+                <label for="password">Senha</label>
+                @if ($errors->has('password'))                    
+                    <span class="helper-text" data-error="{{ $errors->first('password') }}"></span>
+                @endif
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="input-field col s12">
+                <i class="fa fa-lock prefix" aria-hidden="true"></i> 
+                <input name="password_confirmation" type="password" class="validate white-text">
+                <label for="password_confirmation">Confirmar senha</label>
+                @if ($errors->has('password_confirmation'))                    
+                    <span class="helper-text" data-error="{{ $errors->first('password_confirmation') }}"></span>
+                @endif
+            </div>
+        </div>
+
+        <button type="submit" class="btn btn-registrar">
+            <i class="fa fa-btn fa-user"></i> Registrar
+        </button>
+       
+    </div>
+</div>
+@endsection
+
+{{--  @extends('template-guest')
 @include('auth.style')
 @section('conteudo')
 
@@ -76,4 +137,4 @@
         </div>
     </div>
 </div>
-@endsection
+@endsection  --}}
