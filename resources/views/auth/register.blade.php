a1b3d4259a50e2867e35b08cf2d5b25ee32fbce0
Marcos Freire <marcos.freire@serpro.gov.br>
2018-03-20 09:17:38 -0300

    Ajuste formulário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
 
             {{--  {{$errors}}  --}}
             <div class="row">
-                <div class="input-field col s6">
+                <div class="input-field col m6 s12">
                     <i class="fa fa-user prefix" aria-hidden="true"></i>
                     <input id="name" name="name" type="text"
                          class="validate white-text {{ $errors->has('name') ? 'invalid' : '' }}">
@@ -23,7 +23,7 @@
                     @endif
                 </div>
                 
-                <div class="input-field col s6">
+                <div class="input-field col m6 s12">
                     <i class="fa fa-envelope prefix" aria-hidden="true"></i>
                     <input id="email" name="email" type="email" 
                         class="validate white-text {{ $errors->has('email') ? 'invalid' : '' }}">
@@ -32,7 +32,7 @@
                     <span class="helper-text" data-error="{{ $errors->first('email') }}"></span>
                     @endif
                 </div>
-                <div class="input-field col s6">
+                <div class="input-field col m6 s12">
                     <i class="fa fa-lock prefix" aria-hidden="true"></i>
                     <input name="password" type="password" 
                         class="validate white-text {{ $errors->has('password') ? 'invalid' : '' }}">
@@ -41,7 +41,7 @@
                     <span class="helper-text" data-error="{{ $errors->first('password') }}"></span>
                     @endif
                 </div>
-                <div class="input-field col s6">
+                <div class="input-field col m6 s12">
                     <i class="fa fa-lock prefix" aria-hidden="true"></i>
                     <input name="password_confirmation" type="password" 
                         class="validate white-text {{ $errors->has('password_confirmation') ? 'invalid' : '' }}">
@@ -49,10 +49,8 @@
                     @if ($errors->has('password_confirmation'))
                     <span class="helper-text" data-error="{{ $errors->first('password_confirmation') }}"></span>
                     @endif
-                </div>
-                
-            </div>            
-
+                </div>                
+            </div>    
 
             <div class="row center-align">
                 <button type="submit" class="btn green darken-1">
@@ -63,72 +61,4 @@
 
     </div>
 </div>
-@endsection {{-- @extends('template-guest') @include('auth.style') @section('conteudo')
-
-<div class="formulario">
-    <div class="row">
-        <div class="panel-heading">Registrar novo usuário</div>
-        <div class="panel-body">
-            <form class="form-horizontal" role="form" method="POST" action="{{ url('/register') }}">
-                {!! csrf_field() !!}
-
-                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Nome</label>
-
-                    <div class="col-campo">
-                        <input type="text" class="form-control" name="name" value="{{ old('name') }}"> @if ($errors->has('name'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('name') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Email</label>
-
-                    <div class="col-campo">
-                        <input type="email" class="form-control" name="email" value="{{ old('email') }}"> @if ($errors->has('email'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('email') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Senha</label>
-
-                    <div class="col-campo">
-                        <input type="password" class="form-control" name="password"> @if ($errors->has('password'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Confirmar senha</label>
-
-                    <div class="col-campo">
-                        <input type="password" class="form-control" name="password_confirmation"> @if ($errors->has('password_confirmation'))
-                        <span class="help-block">
-                            <strong>{{ $errors->first('password_confirmation') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group">
-                    <div class="col-campo col-md-offset-4">
-                        <button type="submit" class="btn btn-registrar">
-                            <i class="fa fa-btn fa-user"></i> Registrar
-                        </button>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-@endsection --}}
+@endsection 
