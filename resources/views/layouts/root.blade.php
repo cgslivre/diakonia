8935a2c12d86c349cbb6807eb47f411e1969fd6e
Marcos Freire <marcos.freire@serpro.gov.br>
2018-03-21 10:03:24 -0300

    Favicon

--- resources/views/layouts/root.blade.php
+++ resources/views/layouts/root.blade.php
@@ -7,11 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge"> {{-- Diz ao browser para ser responsivo com a largura da tela --}}
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <title>Diakonia - Discípulos em Brasília - @yield('titulo')</title>
-    {{--
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">--}}
-    <link rel="icon" href="{{asset('img/favicon-32px.png')}}" />
-    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
-    <link rel="icon" href="{{asset('img/favicon.ico')}}" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <link rel="stylesheet" href="{{ mix('/css/style.css') }}">
 </head>
 
