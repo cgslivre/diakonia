3389622426a426ae0646d9972e14e3c8cff8f9c2
Marcos Freire <marcos.freire@serpro.gov.br>
2018-03-23 18:56:51 -0300

    Ajuste logo topo

--- resources/views/layouts/app/top.blade.php
+++ resources/views/layouts/app/top.blade.php
@@ -1,50 +1,46 @@
 <header id="header" class="page-topbar">
-    <div class="navbar-fixed">
-        <nav class="gradient-blue">
-            <div class="nav-wrapper">
-                <div>
-                    <a href="#" class="brand-logo center hide-on-med-and-down">
-                        <img src="{{asset('img/logotipo-white-h50px.png')}}" alt="Diakonia Logo">
+    <nav class="navbar-fixed">
+        <a href="#" class="brand-logo center hide-on-med-and-down">
+            <img src="{{asset('img/logotipo-white-h50px.png')}}" alt="Diakonia Logo">
+        </a>
+        <a href="#" class="brand-logo-mobile center show-on-medium-and-down hide-on-large-only">
+            <img src="{{asset('img/logotipo-white-h50px.png')}}" alt="Diakonia Logo">
+        </a>
+        <div class="nav-wrapper">
+            <ul id="status-menu" class="right hide-on-med-and-down">
+
+                <li>
+                    <a href="javascript:void(0);" class="dropdown-trigger waves-effect waves-block waves-light profile-button" data-target="profile-dropdown">
+                        <span class="avatar-status">
+                            <img src="../images/avatar.png" alt="avatar">
+                        </span>
+                        <span class="profile-name">Marcos</span>
                     </a>
-                    <a href="#" class="brand-logo-mobile center show-on-medium-and-down hide-on-large-only">
-                        <img src="{{asset('img/logotipo-white-h50px.png')}}" alt="Diakonia Logo">
-                    </a>
-                </div>
-                <ul id="status-menu" class="right hide-on-med-and-down">
-                    
-                    <li>
-                        <a href="javascript:void(0);" class="dropdown-trigger waves-effect waves-block waves-light profile-button" data-target="profile-dropdown">
-                            <span class="avatar-status">
-                                <img src="../images/avatar.png" alt="avatar">                                
-                            </span>
-                            <span class="profile-name">Marcos</span> 
-                        </a>
-                        <ul id="profile-dropdown" class="dropdown-content" style="white-space: nowrap; opacity: 1; left: 1392.64px; position: absolute; top: 64px; display: none;">
-                            <li>
-                                <a href="#" class="grey-text text-darken-1">
-                                    <i class="material-icons">face</i> Profile</a>
-                            </li>
-                            <li>
-                                <a href="#" class="grey-text text-darken-1">
-                                    <i class="material-icons">settings</i> Settings</a>
-                            </li>
-                            <li>
-                                <a href="#" class="grey-text text-darken-1">
-                                    <i class="material-icons">live_help</i> Help</a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <a href="#" class="grey-text text-darken-1">
-                                    <i class="material-icons">lock_outline</i> Lock</a>
-                            </li>
-                            <li>
-                                <a href="#" class="grey-text text-darken-1">
-                                    <i class="material-icons">keyboard_tab</i> Logout</a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </div>
+                    <ul id="profile-dropdown" class="dropdown-content" style="white-space: nowrap; opacity: 1; left: 1392.64px; position: absolute; top: 64px; display: none;">
+                        <li>
+                            <a href="#" class="grey-text text-darken-1">
+                                <i class="material-icons">face</i> Profile</a>
+                        </li>
+                        <li>
+                            <a href="#" class="grey-text text-darken-1">
+                                <i class="material-icons">settings</i> Settings</a>
+                        </li>
+                        <li>
+                            <a href="#" class="grey-text text-darken-1">
+                                <i class="material-icons">live_help</i> Help</a>
+                        </li>
+                        <li class="divider"></li>
+                        <li>
+                            <a href="#" class="grey-text text-darken-1">
+                                <i class="material-icons">lock_outline</i> Lock</a>
+                        </li>
+                        <li>
+                            <a href="#" class="grey-text text-darken-1">
+                                <i class="material-icons">keyboard_tab</i> Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
 </header>
